7198ffce92cb1ba2e24577aca8b160d559a1851b
Igor K <k.iv86@yandex.ru>
2022-02-07 21:17:06 +0300

    rename main class

--- src/main/java/com/github/KIV86/CarTravel/app/CarTravelApp.java
+++ src/main/java/com/github/KIV86/CarTravel/app/CarTravelApp.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppApplication {
+public class CarTravelApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
+		SpringApplication.run(CarTravelApp.class, args);
+		System.out.println("Инициализация проекта");
 	}
 
 }
