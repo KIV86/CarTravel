c9fcc7a23758b9574c35135f2c75aa4064a6ae64
Igor K <k.iv86@yandex.ru>
2022-02-07 22:03:28 +0300

    Сгенерирован токен для телеграмм бота, добавлен в проект

---
+++ src/main/java/com/github/KIV86/CarTravel/app/common/Token.java
@@ -0,0 +1,10 @@
+package com.github.KIV86.CarTravel.app.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Token {
+    private String token = "2123329401:AAHFiNUuFffg-XaP3f9mIQZbajp2LMclbOA";
+}
